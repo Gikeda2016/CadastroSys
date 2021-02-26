e4ae9e21e2b2c0628b5488f22e5b3cb7d0472196
George Ikeda <75345627+Gikeda2016@users.noreply.github.com>
2021-02-25 23:54:20 -0300

    Update __init__.py

--- Lib/valida/__init__.py
+++ Lib/valida/__init__.py
@@ -14,9 +14,6 @@ inversion_ ='\33[7;37;40m' ## padrão reverso -  fundo branco, letra preta
 title_ = '\33[1;33m' ## padrão  -  fundo preto, letra branca
 
 
-
-
-
 def leiainteiro(mensag):
     ''' Valida o dado entrada para formato int
     '''
@@ -33,10 +30,10 @@ def leiainteiro(mensag):
             print(f'{red_} ERRO: por favor, digite um valor válido.{default_}')
             print()
             continue
-
         else:
             break
     return num
+
 
 def leiastring(mensag):
     ''' valida o dado de entrada para o formato string '''
